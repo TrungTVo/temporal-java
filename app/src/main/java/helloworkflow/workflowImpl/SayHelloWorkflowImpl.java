1de23b249273c9906c50e48f0d406ba2f3cdd8a8
Trung Vo <vtt311096@gmail.com>
2026-07-03 21:46:26 -0500

    minor update

--- app/src/main/java/helloworkflow/workflowImpl/SayHelloWorkflowImpl.java
+++ app/src/main/java/helloworkflow/workflowImpl/SayHelloWorkflowImpl.java
@@ -14,7 +14,7 @@ public class SayHelloWorkflowImpl implements SayHelloWorkflow {
 
     private String greetingStatus = "Greeting has not started yet.";
 
-    private final GreetActivities activities = Workflow.newActivityStub(
+    private final GreetActivities greetActivities = Workflow.newActivityStub(
             GreetActivities.class,
             ActivityOptions.newBuilder()
                     .setStartToCloseTimeout(Duration.ofSeconds(15))
@@ -29,7 +29,7 @@ public class SayHelloWorkflowImpl implements SayHelloWorkflow {
         Workflow.sleep(Duration.ofSeconds(10));
         greetingStatus = "Greeting is in progress for " + request.name() + ".";
         try {
-            String greeting = activities.greet(request.name(), request.shouldFail());
+            String greeting = greetActivities.greet(request.name(), request.shouldFail());
             greetingStatus = "Greeting completed for " + request.name() + ".";
             return greeting;
         } catch (RuntimeException e) {
